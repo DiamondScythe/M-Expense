e8c6c147e4a9e67495054c60a337bd1c5098155c
nhutoan <nhutoan399@gmail.com>
2022-10-11 17:46:58 +0700

    some stuff

--- app/src/main/java/com/example/mexpense/TripDetailFragment.kt
+++ app/src/main/java/com/example/mexpense/TripDetailFragment.kt
@@ -5,8 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import com.example.mexpense.data.trip.Trip
 
 class TripDetailFragment : Fragment() {
+
+    lateinit var trip: Trip
+    private val viewModel: TripViewModel by activityViewModels{
+        TripViewModelFactory(
+            //Use the database instance you created in one of the previous tasks to call the itemDao constructor.
+            (activity?.application as MExpenseApplication).database.tripDao()
+        )
+    }
+
     private val navigationArgs: TripDetailFragmentArgs by navArgs()
 }
