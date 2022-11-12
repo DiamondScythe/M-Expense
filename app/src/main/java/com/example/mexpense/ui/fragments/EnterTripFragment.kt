eaa46b965dd1ecb718f1706c1754624b215b0450
nhutoan <nhutoan399@gmail.com>
2022-11-12 17:56:14 +0700

    added functionality to entertripfragment: Get current location (for now it gets country only)

--- app/src/main/java/com/example/mexpense/ui/fragments/EnterTripFragment.kt
+++ app/src/main/java/com/example/mexpense/ui/fragments/EnterTripFragment.kt
@@ -1,8 +1,12 @@
 package com.example.mexpense.ui.fragments
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,7 +16,6 @@ import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.mexpense.data.trip.Trip
@@ -21,6 +24,10 @@ import com.example.mexpense.MExpenseApplication
 import com.example.mexpense.R
 import com.example.mexpense.TripViewModel
 import com.example.mexpense.TripViewModelFactory
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -28,6 +35,12 @@ import java.util.*
 class EnterTripFragment : Fragment() {
     private var _binding: FragmentEnterTripBinding? = null
     private val binding get() = _binding!!
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    //init the variables lat for latitude and long for longitude
+    private var lat: Double = 0.0
+    private var long: Double = 0.0
+
     private val viewModel: TripViewModel by activityViewModels {
         TripViewModelFactory(
             //Use the database instance you created in one of the previous tasks to call the itemDao constructor.
@@ -45,10 +58,8 @@ class EnterTripFragment : Fragment() {
                 R.id.riskCheckBox -> {
                     if (checked) {
                         tripRisk = "Yes"
-                        Toast.makeText(context, "checked", Toast.LENGTH_SHORT).show()
                     } else {
                         tripRisk = "No"
-                        Toast.makeText(context, "unchecked", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
@@ -81,7 +92,6 @@ class EnterTripFragment : Fragment() {
                 tripRisk,
                 binding.tripDescription.text.toString(),
             )
-            Toast.makeText(requireContext(), "Trip added!", Toast.LENGTH_SHORT).show()
             findNavController().navigate(action)
         }
         else{
@@ -91,18 +101,99 @@ class EnterTripFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //location stuff here
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        setCoordinates()
+
         binding.apply {
             binding.ButtonEnter.setOnClickListener {
                 addNewItem()
             }
-            binding.tripTime.transformIntoDatePicker(requireContext(), "MM/dd/yyyy", Date())
+            binding.tripTime.transformIntoDatePicker(requireContext(), "MM/dd/yyyy")
             binding.riskCheckBox.setOnCheckedChangeListener { view, _ ->
                 onCheckboxClicked(view)
+            }
+            binding.getLocationButton.setOnClickListener {
+                setCoordinates()
+                getUserLocation()
             }
         }
     }
 
-    fun EditText.transformIntoDatePicker(context: Context, format: String, maxDate: Date? = null) {
+    private fun getUserLocation() {
+        if (!checkLocationPermission()){
+            //if user hasn't granted location permission yet, do this
+            getPermission()
+            Toast.makeText(requireContext(), "Location permission has not been granted", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            val currentLat = lat
+            val addressInfo = getAddressInfo(lat, long)
+            if (addressInfo == "Location initializing"){
+                Toast.makeText(requireContext(), "Location services initializing, please try again.", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                binding.tripLocation.setText(addressInfo)
+            }
+        }
+    }
+
+    private fun getPermission() {
+        //if permission isn't granted, asks the user for it
+        if (ActivityCompat.checkSelfPermission(requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                101)
+        }
+    }
+
+
+    private fun setCoordinates() {
+        val priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        val cancellationTokenSource = CancellationTokenSource()
+
+        if (ActivityCompat.checkSelfPermission(requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        //first, checks if the location service toggle is enabled on the device. If enabled, get the
+        //current location and pass it to the location variables using setCoordinates()
+        if (isLocationEnabled(requireContext())) {
+            fusedLocationProviderClient.getCurrentLocation(
+                priority, cancellationTokenSource.token
+            )
+                .addOnSuccessListener { location ->
+                    setCoordinates(location.latitude, location.longitude)
+                }
+        }
+        return
+    }
+
+    private fun setCoordinates(thisLat: Double, thisLong: Double){
+        lat = thisLat
+        long = thisLong
+    }
+
+    //checks if the location service toggle is enabled
+    private fun isLocationEnabled(mContext: Context): Boolean {
+        val lm = mContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return lm.isProviderEnabled(LocationManager.GPS_PROVIDER) || lm.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun EditText.transformIntoDatePicker(context: Context, format: String) {
         isFocusableInTouchMode = false
         isClickable = true
         isFocusable = false
@@ -123,7 +214,6 @@ class EnterTripFragment : Fragment() {
                     .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                 myCalendar.get(Calendar.DAY_OF_MONTH)
             ).run {
-                maxDate?.time?.also { datePicker.maxDate = it }
                 show()
             }
         }
@@ -143,18 +233,16 @@ class EnterTripFragment : Fragment() {
         return true
     }
 
-    private fun getPermission() {
-        //if permission isn't granted, ask the user for it
-        if (ActivityCompat.checkSelfPermission(requireContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(requireContext(),
-                android.Manifest.permission.ACCESS_COARSE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                101)
+    private fun getAddressInfo(lat: Double, long:Double): String{
+        if (lat != 0.0){
+            val geocoder = Geocoder(requireContext(), Locale.getDefault())
+            val address = geocoder.getFromLocation(lat, long, 1)
+
+            val country = address!!.first().countryName
+
+            val addressInfo = country
+            return addressInfo
         }
+        return "Location initializing"
     }
 }
--- app/src/main/java/com/example/mexpense/ui/fragments/SelectionFragment.kt
+++ app/src/main/java/com/example/mexpense/ui/fragments/SelectionFragment.kt
@@ -1,12 +1,15 @@
 package com.example.mexpense.ui.fragments
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.mexpense.databinding.FragmentSelectionBinding
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
